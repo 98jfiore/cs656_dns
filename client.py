3ff516e32789dfdcf98c32738b227b4e76db9a55
Joseph Fiore <98jfiore@gmail.com>
2021-02-20 13:13:05 -0500

    Add base code for a UDP client and server

--- client.py
+++ client.py
@@ -1,1 +1,16 @@
+from socket import *
 
+serverName = 'localhost'
+serverPort = 12000
+
+clientSocket = socket(AF_INET, AF_INET)
+
+message = input("Input lowercase sentence: ")
+
+clientSocket.sendto(message.encode(), (serverName, serverPort))
+
+modifiedMessage, serverAddress = clientSocket.recvfrom(2048)
+
+print(modifiedMessage.decode())
+
+clientSocket.close()
