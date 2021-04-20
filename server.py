6e5d6b156f1ac954226a669531558ecc5351014d
Joseph Fiore <98jfiore@gmail.com>
2021-04-20 12:39:05 -0400

    Add proper encoding of everything

--- server.py
+++ server.py
@@ -17,71 +17,167 @@ print("The server is ready to receive")
 
 while True:
     message, clientAddress = serverSocket.recvfrom(2048)
+    recursion = False
     #print(message)
-    queryencode = (message[12:-4])
-    queryencode2 = (message[11:-4])
-    strqueryencode = str(message[12:-4])
-    pattern = r'[0-9]'
-    querydecode = re.sub(pattern, '.', strqueryencode)
-    querystrip = querydecode.split("'.")[1]
-    query = querystrip.split(".'")[0]
     #print(queryencode2)
 
     found = False
 
-    for record in readtext:
-        if query in record:
-            answer = (record.split("\n")[0])
-            ttl = int(answer.split(" ")[1]) #59
-            clas = answer.split(" ")[2] #IN
-            query_type = answer.split(" ")[3] #A
-            data = answer.split(" ")[4] #208.56.54.454
-            a = int(data.split(".")[0])
-            b = int(data.split(".")[1])
-            c = int(data.split(".")[2])
-            d = int(data.split(".")[3])
-            qlenght = len(data) #10
+    #Get identification number
+    response_ident = int.from_bytes(message[0:2], "big")
 
-            #print(clas)
-            #print(ttl)
-            #print(a,b,c,d)
+    request_flags = int.from_bytes(message[2:4], "big")
+    #Cheack request flags
+    #Rcode should be 0, if not drop
+    if request_flags & 15 > 0:
+        continue
+    request_flags = request_flags >> 4
+    #Move over zero field
+    request_flags = request_flags >> 3
+    #Next bit is recusion available, this is hopefully a request so ignore
+    request_flags = request_flags >> 1
+    #Next bit is recusion desired
+    if request_flags & 1 > 0:
+        recursion = True
+    request_flags = request_flags >> 1
+    #Next bit is truncated, this is for udp, ignore it
+    request_flags = request_flags >> 1
+    #Next bit is authoritative answer, this is hopefully a request so ignore
+    request_flags = request_flags >> 1
+    #We're only set up to handle opcode 0
+    if request_flags & 15 > 0:
+        continue
+    request_flags = request_flags >> 4
+    #Next bit is QR, it should be 0, for query
+    if request_flags & 1 > 0:
+        continue
+    request_flags = request_flags >> 1
 
-            clasencode= clas.encode()
-            typeencode= query_type.encode()
-            dataencode = data.encode()
-            #ttlencode = repr(ttl).encode('utf-8')
-            #qenconde = repr(qlenght).encode('utf-8')
+    #Set up the 16 bits of response_flags
+    #If response make first bit 1, request make it 0
+    response_flags = 1
+    #Opcode is next 4 bits, we are doing a standard query so it is 0
+    response_flags = response_flags << 4
+    #Next bit is 1 if this is an authoritative name server, which it is
+    response_flags += 1
+    response_flags = response_flags << 1
+    #Next bit is to indicate if the reply is truncated, this is not
+    response_flags = response_flags << 1
+    #Next bit is if you want recursion from the server, this is a response
+    response_flags = response_flags << 1
+    #Next bit is if the response has recursion available, this is not yet implemented
+    response_flags = response_flags << 1
+    #Next 3 nbits must be zero
+    response_flags = response_flags << 3
+    #Next 4 bits are for the return code, set to zero, but set to 3 if the name isn't found
+    response_flags = response_flags << 4
 
+    #Figure out how many questions there are
+    num_questions = int.from_bytes(message[4:6], "big")
 
-            class_typepack = struct.pack("!ss", typeencode, clasencode)
-            print(class_typepack)
+    #There should be no RRs.
 
-            querypack = struct.pack("!{}s".format(len(queryencode2)), queryencode2)
-            print(querypack)
+    response_first_header = response_ident
+    response_first_header = response_first_header << 16
+    response_first_header += response_flags
+    rfh_bytes = response_first_header.to_bytes(4, 'big')
 
-            ttl_len_datapack = struct.pack("!iiiiii", ttl, qlenght, a, b, c, d)
-            print(ttl_len_datapack)
+    num_answers = 0
+    answers = b''
 
-            #ttlpack = struct.pack("!i", ttl)
-            #print(ttlpack)
+    queries = message[12:]
+    for i in range(num_questions):
+        #Get the query name you are looking for
+        query_name = ""
+        point_in_query = 1
+        qname_next_sect_bytes = int.from_bytes(queries[0:1], "big")
+        while(qname_next_sect_bytes != 0):
+            #print(query_name)
+            query_next_sect = queries[point_in_query : point_in_query + qname_next_sect_bytes].decode('utf-8')
+            query_name += query_next_sect
+            query_name += '.'
+            point_in_query += qname_next_sect_bytes + 1
+            qname_next_sect_bytes = int.from_bytes(queries[point_in_query - 1:point_in_query], "big")
+        query_name = query_name[0:-1]
+        #print(query_name)
+        
+        queryencoded = queries[0:point_in_query]
+        queries = queries[point_in_query:]
 
-            #lenpack = struct.pack("!i", qlenght)
-            #print(lenpack)
+        query_type = int.from_bytes(queries[0:2], 'big')
+        query_class = int.from_bytes(queries[2:4], 'big')
+        queries = queries[4:]
+        
+        #We only have A records, so the if type's not 1, we won't check.
+        #We also only return IP addresses so if class isn't 1, we won't check
+        if(query_type == 1 and query_class == 1):
+            for record in readtext:
+                if query_name in record:
+                    answer = (record.split("\n")[0])
+                    ttl = int(answer.split(" ")[1]) #59
+                    clas = answer.split(" ")[2] #IN
+                    query_type = answer.split(" ")[3] #A
 
-            #datapack = struct.pack("!iiii", a, b, c, d)
-            #print(datapack)
+                    #print(clas)
+                    #print(ttl)
+                    #print(a,b,c,d)
 
-            #pack_all = querypack + class_typepack + ttlpack + lenpack + datapack
-            pack_all = querypack + class_typepack + ttl_len_datapack
-            print(pack_all)
+                    answer_bytes = queryencoded
+                    resource_bytes = b''
 
-            serverSocket.sendto(pack_all, clientAddress)
+                    resource_type = 0
+                    resource_class = 0
+                    resource_length = 0
 
-            found = True
-            break
-    if not found:
-        answer = query
-        serverSocket.sendto(answer.encode(), clientAddress)
+                    if(query_type == 'A'):
+                        resource_type = 1
+                        resource_length = 4
+                        data = answer.split(" ")[4] #208.56.54.454
+                        a = int(data.split(".")[0])
+                        b = int(data.split(".")[1])
+                        c = int(data.split(".")[2])
+                        d = int(data.split(".")[3])
+                        resource_bytes = a.to_bytes(1, 'big')
+                        resource_bytes += b.to_bytes(1, 'big')
+                        resource_bytes += c.to_bytes(1, 'big')
+                        resource_bytes += d.to_bytes(1, 'big')
+                    else:
+                        #We are only working with A types now
+                        continue
+
+                    answer_bytes += resource_type.to_bytes(2, 'big')
+
+                    if(clas == 'IN'):
+                        resource_class = 1
+                    else:
+                        #We are only working with IN class answers now
+                        continue
+
+                    answer_bytes += resource_class.to_bytes(2, 'big')
+
+                    answer_bytes += ttl.to_bytes(4, 'big')
+
+                    answer_bytes += resource_length.to_bytes(2, 'big')
+
+                    answer_bytes += resource_bytes
+
+                    answers += answer_bytes
+                    num_answers += 1
+
+                    break
+
+    response_sec_header = 0
+    response_sec_header = response_sec_header << 16
+    response_sec_header += num_answers
+    rsh_bytes = response_sec_header.to_bytes(4, 'big')
+
+    response_third_header = 0
+    rth_bytes = response_third_header.to_bytes(4, 'big')
+
+    response_bytes = rfh_bytes + rsh_bytes + rth_bytes + answers
+
+    serverSocket.sendto(response_bytes, clientAddress)
+    
 
 #NAME
 #TYPE
